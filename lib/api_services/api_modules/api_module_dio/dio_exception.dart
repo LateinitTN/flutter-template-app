9c71039b63f6042d85c761315820ed27679ef620
Theanun <theanunr@gmail.com>
2021-01-20 09:56:08 +0700

    clean up dio exception

--- lib/api_services/api_modules/api_module_dio/dio_exception.dart
+++ lib/api_services/api_modules/api_module_dio/dio_exception.dart
@@ -13,38 +13,3 @@ class FetchDataException extends AppException {
   FetchDataException([String message])
       : super(message, "");
 }
-
-abstract class BaseHttpException {}
-
-class DioErrorException extends BaseHttpException {
-  final int code;
-  final String message;
-
-  DioErrorException(this.message, {this.code = 200});
-  @override
-  String toString() {
-    return this.message;
-  }
-}
-
-class ServerErrorException extends BaseHttpException {
-  final String message;
-
-  ServerErrorException(this.message);
-
-  @override
-  String toString() {
-    return this.message;
-  }
-}
-
-class ServerResponseException extends BaseHttpException {
-  final String message;
-
-  ServerResponseException(this.message);
-
-  @override
-  String toString() {
-    return this.message;
-  }
-}
--- lib/constants/api_constants/api_constants.dart
+++ lib/constants/api_constants/api_constants.dart
@@ -6,14 +6,5 @@ class ApiMethod {
   static const String POST = "post";
 }
 
-
-class ErrorMessage {
-  static const UNEXPECTED_ERROR = "An unexpected error occur!";
-  static const CONNECTION_ERROR =
-      "Error connecting to server. Please check your internet connection or Try again later!";
-  static const TIMEOUT_ERROR =
-      "Connection timeout. Please check your internet connection or Try again later!";
-}
-
 //Routes
 const String getAlbums = "albums";
